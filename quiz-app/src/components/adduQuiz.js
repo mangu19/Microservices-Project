0b60a10671de8c604cbbd77a300711f278f7d215
Mangesh Sawant <mangeshsawant608@gmail.com>
2024-03-14 23:59:41 +0530

    add axios

---
+++ quiz-app/src/components/adduQuiz.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function adduQuiz() {
+  return (
+    <div>
+      <h2>Add Quiz</h2>
+      dfguhjkjuytdsfxgchjbklkuydsfxcvbhjkljjhgf
+    </div>
+  );
+}
